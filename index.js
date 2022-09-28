6ee84e3fbd709268a27ddb4d25bd689d7064d14d
Rafał Pachnicki <rafal.pachnicki@hotmail.com>
2022-09-28 22:28:16 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let hatPrice = 100;
+console.log(`Cena czapki: ${hatPrice}`);
+let bootsPrice = "100";
+console.log(`Cena butów: ${bootsPrice}`);
+
+if (hatPrice == bootsPrice) {
+   console.log("Ceny są takie same");
+} else {
+   console.log("Ceny są różne");
+}
+
+let totalPrice = hatPrice + bootsPrice;
+console.log(`Cena całkowita: ${totalPrice}`);
+
+let myVariable = "Rafał";
+console.log(`Typ: ${typeof myVariable}`);
+myVariable = 100;
+console.log(`Typ: ${typeof myVariable}`);
