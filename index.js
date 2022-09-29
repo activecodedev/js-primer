3ae2ecb5ce119e835d2416e06b7c35e4321c78d2
Rafał Pachnicki <rafal.pachnicki@hotmail.com>
2022-09-29 11:40:57 +0200

    Third commit

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ console.log(`Cena czapki: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Cena butów: ${bootsPrice}`);
 
-function sumPrices(first, second, third) {
-   return first + second + third;
+function sumPrices(...numbers) {
+   return numbers.reduce(function (total, val) {
+      let currentValue = Number(val);
+      let afterCastToNumber = Number.isNaN(currentValue) ? 0 : currentValue;
+
+      return total + afterCastToNumber;
+   }, 0);
 }
 
 let totalPrice = sumPrices(hatPrice, bootsPrice);
-console.log(`Kwota całkowita: ${totalPrice}`);
+console.log(`Wartość całkowita: ${totalPrice} ${typeof totalPrice}`);
+totalPrice = sumPrices(100, 200, 300);
+console.log(`Wartość całkowita: ${totalPrice} ${typeof totalPrice}`);
+totalPrice = sumPrices(100, 200, undefined, false, "Witaj");
+console.log(`Wartość całkowita: ${totalPrice} ${typeof totalPrice}`);
 
 let myVariable = "Rafał";
 console.log(`Typ: ${typeof myVariable}`);
