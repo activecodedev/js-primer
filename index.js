226fe10a1ff73b3233982c2b30ac71d0804c059e
Rafał Pachnicki <rafal.pachnicki@hotmail.com>
2022-09-29 12:50:40 +0200

    Fourth commit

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
-let hatPrice = 100;
-console.log(`Cena czapki: ${hatPrice}`);
-let bootsPrice = "100";
-console.log(`Cena butów: ${bootsPrice}`);
+let hat = {
+   name: "czapka",
+   price: 100
+};
+let boots = {
+   name: "buty",
+   price: "100"
+};
+let gloves = {
+   productName: "rękawiczki",
+   price: "40"
+};
 
-function sumPrices(...numbers) {
-   return numbers.reduce(function (total, val) {
-      let currentValue = Number(val);
-      let afterCastToNumber = Number.isNaN(currentValue) ? 0 : currentValue;
+gloves.name = gloves.productName;
+delete gloves.productName;
+gloves.price = 20;
 
-      return total + afterCastToNumber;
-   }, 0);
-}
+let propertyCheck = hat.price || 0;
+let objectAndPropertyCheck = (hat || {}).price || 0;
+console.log(`Sprawdzenie: ${propertyCheck}, ${objectAndPropertyCheck}`);
 
-let totalPrice = sumPrices(hatPrice, bootsPrice);
+let names = ["czapka", "buty", "rękawiczki"];
+let prices = [];
+
+prices.push(100);
+prices.push("100");
+prices.push(50.25);
+console.log(`Pierwszy element: ${names[0]}: ${prices[0]}`);
+
+let sumPrices = (...numbers) => numbers.reduce((total, val) =>
+   total + (Number.isNaN(Number(val)) ? 0 : Number(val)), 0);
+
+let totalPrice = sumPrices(...prices);
 console.log(`Wartość całkowita: ${totalPrice} ${typeof totalPrice}`);
+
+totalPrice = sumPrices(hat.price, boots.price, gloves.price);
+console.log(`Wartość całkowita: ${totalPrice} ${typeof totalPrice}`);
+
+let combinedArray = [...names, ...prices];
+combinedArray.forEach(element => console.log(`Połączony element tablicy: ${element}`));
+
 totalPrice = sumPrices(100, 200, 300);
 console.log(`Wartość całkowita: ${totalPrice} ${typeof totalPrice}`);
 totalPrice = sumPrices(100, 200, undefined, false, "Witaj");
